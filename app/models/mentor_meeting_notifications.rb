6cdab8daa248e65da925df90af6e93e9e7a1ae1b
Anusha Akkireddy <41582420+akkireddyanusha@users.noreply.github.com>
2024-10-29 17:48:59 -0400

    Update mentor_meeting_notifications.rb

--- app/models/mentor_meeting_notifications.rb
+++ app/models/mentor_meeting_notifications.rb
@@ -4,8 +4,6 @@ class MentorMeetingNotifications
   end
 end
 
-# Subscribe to the notification and define what action to take
 ActiveSupport::Notifications.subscribe('mentor_meeting.created') do |name, start, finish, id, payload|
   puts "New meeting created for team #{payload[:team_id]} on #{payload[:meeting_date]}"
-  # Here, you can replace the notification logic to send an email, SMS, etc.
 end
