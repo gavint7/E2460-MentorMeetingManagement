a93285cbd5426c49b00e7b0167505f8eab24476f
Simon Getahun <simongetahun@Simons-MBP.attlocal.net>
2024-10-29 21:57:56 -0400

    changed model to handle dual case

--- app/models/mentored_team.rb
+++ app/models/mentored_team.rb
@@ -14,12 +14,18 @@ class MentoredTeam < AssignmentTeam
           ExpertizaLogger.info LoggerMessage.new('Model:Team', user.name, "Added member to the team #{id}")
          
           assignment_name = _assignment_id ? Assignment.find(_assignment_id).name.to_s : ""
-              if MentorManagement.user_a_mentor?(user)
-              MailerHelper.send_team_confirmation_mail_to_user(user, "[Expertiza] Added to a Team", "mentor_added_to_team", "#{name}", assignment_name).deliver
-              elsif !user.is_a?(Participant)
-              MailerHelper.send_team_confirmation_mail_to_user(user, "[Expertiza] Added to a Team", "user_added_to_team", "#{name}", assignment_name).deliver
-          end   
+          # mentor only
+          if MentorManagement.user_a_mentor?(user) && !user.is_a?(Participant)
+            MailerHelper.send_team_confirmation_mail_to_user(user, "[Expertiza] Added to a Team", "mentor_added_to_team", "#{name}", assignment_name).deliver
+          # participant only
+          elsif !MentorManagement.user_a_mentor?(user) && user.is_a?(Participant)
+            MailerHelper.send_team_confirmation_mail_to_user(user, "[Expertiza] Added to a Team", "user_added_to_team", "#{name}", assignment_name).deliver
+          # dual case
+          elsif MentorManagement.user_a_mentor?(user) && user.is_a?(Participant)
+            MailerHelper.send_team_confirmation_mail_to_user(user, "[Expertiza] Added to a Team", "dual_role_added_to_team", "#{name}", assignment_name).deliver
+          end
         end
+
         if can_add_member
             MentorManagement.assign_mentor(_assignment_id, id)
         end
